b4720f2ed432de328047d4c213b969535ad0800c
MikiDani <miklosdani@gmail.com>
2023-10-23 02:16:10 +0200

    webengine-frontend_001

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function AppComponent() {
+  return /*#__PURE__*/React.createElement("h1", {
+    className: 'text-center'
+  }, "Hell\xF3 Valakika3!");
+}
+ReactDOM.render(React.createElement(AppComponent), document.getElementById('app-container'));
